90ac32518f5504a8000b54aef5b05fefd22ebc4a
agatha <agathabos28@gmail.com>
2021-06-10 22:07:56 +0100

    game app and menu/settings/game states

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import pygame
 import skeleton
-background_colour = (128,100,255)
+
+background_colour = (128, 100, 255)
 (width, height) = (700, 700)
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Platformer')
 screen.fill(background_colour)
-skeleton1=skeleton.skeleton()
-skeleton1.setPosition(480,300)
-#skeleton1.walkLeft()
-skeleton1.walkRight()
+skeleton1 = skeleton.skeleton()
+skeleton1.set_position(480, 300)
+# skeleton1.walk_left()
+skeleton1.walk_right()
 pygame.display.flip()
 clock = pygame.time.Clock()
 running = True
 
 while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
-  screen.fill(background_colour)
-  skeleton1.draw(screen)
-  pygame.display.flip()
-  skeleton1.nextFrame()
-  clock.tick(4)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(background_colour)
+    skeleton1.draw(screen)
+    pygame.display.flip()
+    skeleton1.next_frame()
+    clock.tick(4)
--- skeleton.py
+++ skeleton.py
@@ -1,12 +1,13 @@
-import pygame
 import spritesheet
+
 
 class skeleton():
     def __init__(self):
+        self.direction = 1
         ss = spritesheet.spritesheet('skeleton_sheet.png')
-        self.standing = ss.image_at((14, 143, 35, 48),-1)
+        self.standing = ss.image_at((14, 143, 35, 48), -1)
         self.walkingLeft = ss.images_at([
-            ##(15 , 78, 35, 50),
+            # (15 , 78, 35, 50),#
             (79, 78, 35, 50),
             (143, 78, 35, 50),
             (207, 78, 35, 50),
@@ -15,9 +16,9 @@ class skeleton():
             (399, 78, 35, 50),
             (463, 78, 35, 50),
             (527, 78, 35, 50),
-        ],-1)
+        ], -1)
         self.walkingRight = ss.images_at([
-            ##(15 , 78, 35, 50),
+            # (15 , 78, 35, 50),#
             (79, 206, 35, 50),
             (143, 206, 35, 50),
             (207, 206, 35, 50),
@@ -26,25 +27,28 @@ class skeleton():
             (399, 206, 35, 50),
             (463, 206, 35, 50),
             (527, 206, 35, 50),
-        ],-1)
+        ], -1)
         self.currentFrame = 0
         self.currentAnimation = [self.standing]
         self.x = 0
         self.y = 0
-    def draw(self,screen):
-        screen.blit(self.currentAnimation[self.currentFrame],(self.x,self.y))
-    def walkLeft(self):
+
+    def draw(self, screen):
+        screen.blit(self.currentAnimation[self.currentFrame], (self.x, self.y))
+
+    def walk_left(self):
         self.direction=-1
         self.currentAnimation = self.walkingLeft
-    def setPosition(self,x,y):
+
+    def set_position(self,x,y):
         self.x=x
         self.y=y
-    def nextFrame(self):
+
+    def next_frame(self):
         self.currentFrame = self.currentFrame+1
         if self.currentFrame >= len(self.currentAnimation):
             self.currentFrame = 0
         self.x = self.x + (self.direction*3)
-    def walkRight(self):
-        self.direction=1
-        self.currentAnimation = self.walkingRight
 
+    def walk_right(self):
+        self.currentAnimation = self.walkingRight
