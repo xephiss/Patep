bbfaf3152b1eb54b674f87738e998a934d3af79b
agatha <agathabos28@gmail.com>
2021-10-16 09:04:37 +0100

    fixed bg animation  and player stops

--- game_platform.py
+++ game_platform.py
@@ -10,17 +10,17 @@ class Platform(Edges):
 
     def __init__(self, x, y, width, height):
         # initialise the platform tiles and the width,height, x and y of the tiles as well as the number of tiles
-        self.platformTiles = ["13.png", "14.png", "15.png"]
+        self.platform_tiles = ["13.png", "14.png", "15.png"]
         self.x = x
         self.y = y
         self.height = height
         # ensures the platform will always be at least as wide as the specified width
-        self.numHorizontalTiles = math.ceil(width / self.BLOCK_SIZE)
+        self.num_horizontal_tiles = math.ceil(width / self.BLOCK_SIZE)
         # calculates the resulting width of the platform
-        self.width = (self.numHorizontalTiles + 1) * self.BLOCK_SIZE
-        self.rightTile = self.load_tile(self.platformTiles[2])
-        self.middleTile = self.load_tile(self.platformTiles[1])
-        self.leftTile = self.load_tile(self.platformTiles[0])
+        self.width = (self.num_horizontal_tiles + 1) * self.BLOCK_SIZE
+        self.right_tile = self.load_tile(self.platform_tiles[2])
+        self.middle_tile = self.load_tile(self.platform_tiles[1])
+        self.left_tile = self.load_tile(self.platform_tiles[0])
 
     def load_tile(self, filename):
         # load the tiles
@@ -33,16 +33,16 @@ class Platform(Edges):
 
     def draw(self, screen):
         current_pos = self.x  # set the current position
-        num_horizontal_tiles = self.numHorizontalTiles
+        num_horizontal_tiles = self.num_horizontal_tiles
 
-        screen.blit(self.leftTile, (current_pos, self.y))  # draw the left tile on the screen
+        screen.blit(self.left_tile, (current_pos, self.y))  # draw the left tile on the screen
 
         current_pos += self.BLOCK_SIZE  # change the current position to the new position
         # calculate how many tiles will be needed to create a platform in the given space
         if num_horizontal_tiles > 2:
             while num_horizontal_tiles > 1:
                 # draw the middle tiles
-                screen.blit(self.middleTile, (current_pos, self.y))
+                screen.blit(self.middle_tile, (current_pos, self.y))
                 num_horizontal_tiles -= 1
                 current_pos += self.BLOCK_SIZE
-        screen.blit(self.rightTile, (current_pos, self.y))  # draw the right tile
+        screen.blit(self.right_tile, (current_pos, self.y))  # draw the right tile
